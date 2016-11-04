2a9611d75268fabdbe9c6761caf45a529bc6006e
ZiHan <ZiHan@LAPTOP-K3THBP42>
2016-11-05 00:03:31 +0800

    FindPeakElement

--- src/Easy/RemoveDuplicatesFromSortedList.java
+++ src/Easy/RemoveDuplicatesFromSortedList.java
@@ -1,7 +1,17 @@
 package Easy;
 
 import Dependencies.ListNode;
+/*
+ * 
+首先要实现一个单链表，因此需要定义一个节点类Node。
+其次，实现向链表中添加结点的方法(使用尾插法)addNode
 
+删除重复结点的实现思路：
+定义两个指针：pre 和 next。初
+始时，pre指向链表中的第一个元素，next指向链表中的第二个元素。
+如果 pre 的值与 next 的值不相等，则两个指针分别都向后移一个结点；
+若相等，则删除 next 指针指向的结点即可。
+ */
 public class RemoveDuplicatesFromSortedList {
     public ListNode deleteDuplicates(ListNode head) {
         if(head == null || head.next == null)
