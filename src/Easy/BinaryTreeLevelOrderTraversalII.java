0cab02cee07ff7dd9182009702584044d8dada63
ZiHan <ZiHan@LAPTOP-K3THBP42>
2016-11-15 00:30:48 +0800

    Binary Tree Level Order Traversal II

---
+++ src/Easy/BinaryTreeLevelOrderTraversalII.java
@@ -0,0 +1,49 @@
+package Easy;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import Dependencies.TreeNode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class BinaryTreeLevelOrderTraversalII {
+    List<List<Integer>> result = new ArrayList<>();
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        if(root == null)
+            return result;
+            
+        //šv±éß@¿Ã˜ä
+        TraversalTree(root, 0);
+        
+        //Œ¢½Y¹ûµ¹·´
+        Collections.reverse(result);
+        
+        return result;
+    }
+    
+    void TraversalTree(TreeNode root, int level) {
+        if(root == null)
+            return;
+        
+        if(result.size() == level) {
+            result.add(new ArrayList<Integer>());
+        }
+        
+        result.get(level).add(root.val);
+            
+        if(root.left != null)
+            TraversalTree(root.left, level + 1);
+            
+        if(root.right != null)
+            TraversalTree(root.right, level + 1);
+    }
+}
--- src/Medium/FlattenBinaryTreeToLinkedList.java
+++ src/Medium/FlattenBinaryTreeToLinkedList.java
@@ -1,4 +1,4 @@
-package Easy;
+package Medium;
 
 import Dependencies.TreeNode;
 
