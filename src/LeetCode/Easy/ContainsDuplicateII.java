5c0d257c809449dcf6b8843cbeb7823bf31580a3
zihanxu <zihanxu@zihanxu01.sohu-inc.com>
2017-07-12 17:06:03 +0800

    ContainsDuplicateII

--- src/LeetCode/Easy/ContainsDuplicateII.java
+++ src/LeetCode/Easy/ContainsDuplicateII.java
@@ -2,24 +2,24 @@ package LeetCode.Easy;
 
 import java.util.HashMap;
 
-// �鿴�������Ƿ����ظ�Ԫ���������ظ�Ԫ���������������K
+// 查看数组内是否有重复元素且相邻重复元素索引间隔不大于K
 public class ContainsDuplicateII {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         if(nums.length <= 1) {
             return false;
         }
-        //�o�<key, λ��>
+        //紀錄<key, 位置>
         HashMap<Integer, Integer> hashMap = new HashMap<>();
         for(int i = 0; i < nums.length; i ++) {
-            //���������}Ԫ��
+            //若包含重複元素
             if(hashMap.containsKey(nums[i])) {
-                //�z���g���Ƿ���k
+                //檢查間隔是否大於k
                 if((i - hashMap.get(nums[i])) <= k) {
                     return true;
                 }
             }
             
-            //�o�ԓ���cλ��
+            //紀錄該數與位置
             hashMap.put(nums[i], i);
         }
         
