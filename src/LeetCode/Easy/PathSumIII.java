83783935582a3ba23850ab045acb4da03f080bab
zihanxu <zihanxu@zihanxu01.sohu-inc.com>
2017-07-12 17:53:46 +0800

    PathSumIII

--- src/LeetCode/Easy/PathSumIII.java
+++ src/LeetCode/Easy/PathSumIII.java
@@ -13,16 +13,16 @@ import LeetCode.Dependencies.TreeNode;
  */
  
  /*
- ����Ĳ�ͬ����path���Բ���root��ʼ������leaf�����������ڿ��Դ��ڸ����ڵ㣬����û��ͨ���Ƚϴ�С�������ڵ㣬
- �������Ǿ�ֻ�ܿ��Ǵ�ÿһ���ڵ㿪ʼ�������
+ 本题的不同点是path可以不从root开始，不到leaf结束。但由于可以存在负数节点，所以没法通过比较大小来缩进节点，
+ 所以我们就只能考虑从每一个节点开始的情况。
  */
 public class PathSumIII {
     public int pathSum(TreeNode root, int sum) {
         if(root == null)
             return 0;
         
-        //���Ҫ���]ÿ��·��(��һ����root�_ʼ)��
-        //��Ҫ���������3���c�քe�M�а��L
+        //因為要考慮每個路徑(不一定從root開始)，
+        //故要對最上面的3個點分別進行拜訪
         int pathCount = visitPath(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
         return pathCount;
     }
@@ -36,7 +36,7 @@ public class PathSumIII {
         if(root.val == sum)
             pathCount ++;
         
-        //Ŀǰ·���� = ���Ә�·���� + ���Ә�·����
+        //目前路徑數 = 左子樹路徑數 + 右子樹路徑數
         pathCount = pathCount + visitPath(root.left, sum - root.val) + visitPath(root.right, sum - root.val);
         return pathCount;
     }
