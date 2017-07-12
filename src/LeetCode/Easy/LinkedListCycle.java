2487712da5a8a6b0efe680436e64579dd31364ac
zihanxu <zihanxu@zihanxu01.sohu-inc.com>
2017-07-12 17:13:36 +0800

    LinkedListCycle

--- src/LeetCode/Easy/LinkedListCycle.java
+++ src/LeetCode/Easy/LinkedListCycle.java
@@ -12,29 +12,29 @@ import LeetCode.Dependencies.ListNode;
  *         next = null;
  *     }
  * }
- * 
- * �Oһ��fast����һ�ΰ��L�ɂ��c
- * ���Oһ��slow��ֻ��һ�ΰ��Lһ���c
- * ��fast==slow(�ɂ��c����)����ʾ��listNode��h����֮�t����
  */
 public class LinkedListCycle {
     public boolean hasCycle(ListNode head) {
         if(head == null || head.next == null)
             return false;
         
-        //�Ofast�cslow�ɂ�listNode
-        ListNode fast = head.next;
+        ListNode fast = head;
         ListNode slow = head;
         
-        while(fast != slow) {
-            if(fast == null || fast.next == null) {
+        //先確定是否有環
+        while(true) {
+            if(fast == null || fast.next == null)
+            {
+                //遇到null了，说明不存在环
                 return false;
             }
             
             fast = fast.next.next;
             slow = slow.next;
+            
+            if (fast == slow) {
+                return true;    //fast與slow第一次相遇在Z点
+            }
         }
-        
-        return true;
     }
 }
