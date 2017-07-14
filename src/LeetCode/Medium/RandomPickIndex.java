63c55c416754c98a72135f4188b00286635b7de2
zihanxu <zihanxu@zihanxu01.sohu-inc.com>
2017-07-14 10:39:57 +0800

    fdfdf

--- src/LeetCode/Medium/RandomPickIndex.java
+++ src/LeetCode/Medium/RandomPickIndex.java
@@ -7,10 +7,10 @@ int[] nums = new int[] {1,2,3,3,3};
 Solution solution = new Solution(nums);
 
 solution.pick(3); 
-//���e���ҵ�3��index����3��index��2��3�c4���@�rҪ��ͬ���ʵķ���2��3��4
+//從裡面找到3的index，而3的index有2、3與4，這時要相同概率的返回2、3或4
 
 solution.pick(1);
-//���e���ҵ�1��index����1��indexֻ��0���@�rֻҪ����0
+//從裡面找到1的index，而1的index只有0，這時只要返回0
 */
 public class RandomPickIndex {
     int[] nums;
@@ -20,13 +20,13 @@ public class RandomPickIndex {
         this.random = new Random();
     }
     /*
-    ���Ƕ�����������:
-    ����up_bound
-    ���ؽ��result
-    ���Ǳ����������飬��������ֵ������target��ֱ��������
-    �������target����������1��
-    Ȼ��������[0,up_bound]��Χ���������һ�����֣�
-    ������������0�����ǽ�result��ֵΪi����
+    我们定义两个变量:
+    计数up_bound
+    返回结果result
+    我们遍历整个数组，如果数组的值不等于target，直接跳过；
+    如果等于target，计数器加1，
+    然后我们在[0,up_bound]范围内随机生成一个数字，
+    如果这个数字是0，我们将result赋值为i即可
     */
     
     public int pick(int target) {
@@ -35,13 +35,13 @@ public class RandomPickIndex {
         //ˮ����ӿ���kȡ�ӵĹ���
         int up_bound = 1;
         
-        //��v�������M
+        //遍歷整個數組
         for(int i = 0; i < nums.length; i ++) {
             if(nums[i] == target) {
             	//�M��ˮ�����(1/k)�ĸ��ʣ��Ƿ�����@��index
                 if(random.nextInt(up_bound) == 0)
                     result = i; 
-                //��k�Ĺ�������
+                //進行水塘抽樣
                 up_bound ++;
             }
         }
