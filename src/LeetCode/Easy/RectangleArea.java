302ae06d71f97110ed856ef01e96e3175543694e
zihanxu <zihanxu@zihanxu01.sohu-inc.com>
2017-07-14 18:23:59 +0800

    ㄎ

--- src/LeetCode/Easy/RectangleArea.java
+++ src/LeetCode/Easy/RectangleArea.java
@@ -7,21 +7,21 @@ package LeetCode.Easy;
  */
 public class RectangleArea {
     public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
-        //��һ���L���ε���eABCD
+        //第一個長方形的面積ABCD
         int rectangleAArea = Math.abs(A - C) * Math.abs(B - D);
-        //�ڶ����L���ε���eEGFH
+        //第二個長方形的面積EGFH
         int rectangleBArea = Math.abs(E - G) * Math.abs(F - H);
         
-        // ���ABCD�cEGFH�]�دB�������e�t�ǃɂ��L���ε���e
+        // 如果ABCD與EGFH沒重疊，最大面積則是兩個長方形的面積
         if(A >= G || B >= H || C <= E || D <= F) {
             return rectangleAArea + rectangleBArea;
         }
         
-        // Ӌ���دB�Ĳ���
+        // 計算重疊的部分
         int overlapping = 
             Math.abs((Math.max(A, E) - Math.min(C, G)) * (Math.max(B, F) - Math.min(D, H)));
         
-        // ��һ���L����+�ڶ����L����-�دB�t�������e
+        // 第一個長方形+第二個長方形-重疊則是最大面積
         return rectangleAArea + rectangleBArea - overlapping;
     }
 }
