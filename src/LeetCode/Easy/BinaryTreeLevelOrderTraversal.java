8d7dfd9719fbdea80dc60c880aa57d97cdd1e3ae
ZiHan <ff@ys.com>
2017-07-15 21:59:08 +0800

    f

--- src/LeetCode/Easy/BinaryTreeLevelOrderTraversal.java
+++ src/LeetCode/Easy/BinaryTreeLevelOrderTraversal.java
@@ -20,26 +20,26 @@ public class BinaryTreeLevelOrderTraversal {
         if(root == null)
             return result;
         
-        //�Ӕ�, 픹��c
+        //開始進行歷遍
         traversal(0, root);
         
         return result;
     }
     
     public void traversal(int level, TreeNode root) {
-        //����һ��
+        //若當前拜訪的層數在結果中沒有的話就添加
         if(level >= result.size()) {
             result.add(new ArrayList<Integer>());
         }
         
-        //����get�õ�Ŀǰ����v�ČӔ�
+        //依照目前拜訪節點所在的層數添加至結果(結果是按層數來分)
         result.get(level).add(root.val);
         
-        //��v��
+        //拜訪左子樹
         if(root.left != null)
             traversal(level + 1, root.left);
         
-        //�Қv��
+        //拜訪右子樹
         if(root.right != null)
             traversal(level + 1, root.right);
     }
