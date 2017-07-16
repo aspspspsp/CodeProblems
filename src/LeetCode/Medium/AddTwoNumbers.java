32837a02a2797101fdc4798162bdb6546e1457b6
ZiHan <ff@ys.com>
2017-07-16 20:22:28 +0800

    fdsfdsf

--- src/LeetCode/Medium/AddTwoNumbers.java
+++ src/LeetCode/Medium/AddTwoNumbers.java
@@ -9,6 +9,12 @@ import LeetCode.Dependencies.ListNode;
  *     ListNode next; //進到下一位
  *     ListNode(int x) { val = x; } //建構子
  * }
+ * 題目:
+ * 有兩個連結陣列分別代表兩個非負整數，他們的位數是反向儲存(越前面的節點位數越低)，
+ * 毎一個節點代表一個位數，將這兩個連結陣列加總後以連結陣列形式回傳。
+ * 範例：Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 個位數為2與5，相加為7 ； 十位數為4+6 = 10，需要進位 ； 
+ * 百位數為3 + 4 + 1(進位) = 8，結果為 7->0->8
  */
 public class  AddTwoNumbers {	
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
