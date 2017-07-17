1d01d9c00edc1f90403030c6b77293031a420141
zihanxu <zihanxu@zihanxu01.sohu-inc.com>
2017-07-17 10:44:22 +0800

    f

--- src/LeetCode/Medium/UniquePathsII.java
+++ src/LeetCode/Medium/UniquePathsII.java
@@ -3,26 +3,27 @@ package LeetCode.Medium;
 public class UniquePathsII {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         /*
-        ����һ��result��ꇱ�ʾÿһ���е�·����
+    result儲存著動態規劃的結果(每一格都代表到達此點可使用的路徑數量)
         
-        ���ȳ�ʼ��result���
+       先初始化result表格的第0行與第0列
         ex:           
         1 1 1
         1
         1
-        
-        ���� obstacleGrid��ꇵĵ�0�л��0�О�1�r��Ҫ���@һ�����µ�Ԫ�ض��Ğ�0
+    
+	而obstacleGrid代表著障礙物，故result需要配合obstacleGrid做出調整，
+	故要將初始化的第0行第0列根據obstacleGrid的內容改變，例如以下例子:
         ex: 
         obstacleGrid    result
         ------------    ------
-        0 1 0           1 0 0
+        0 1 0           1 0 0 (有障礙物則路徑為0)
         1 0 0       =>  0 
         0 0 0           0
         
-        Ȼ����Mʣ��result�Ĳ���
-        ��obstacleGrid[i][j] = 1�tresult[i][j] = 0
-        ��obstacleGrid[i][j] = 0�tresult[i][j] = result[i - 1][j] + result[i][j - 1]
-        ��ʾ�ϵK�Л]���κ�·��
+ 	動態規劃(result)公式如下:
+ 	若obstacleGrid[i][j] = 1 則 result[i][j] = 0 (有障礙物則路徑為0)
+ 	若obstacleGrid[i][j] = 0 則 result[i][j] = result[i - 1][j] + result[i][j - 1]
+	所以做完動態規劃的結果會如以下所示:
         ex:
         obstacleGrid    result
         ------------    ------
@@ -30,7 +31,7 @@ public class UniquePathsII {
         0 1 0       =>  1 0 1
         0 0 0           1 1 2
         
-        ���᷵��result[n][m]
+ 	返回最後一格的結果result[n][m]
         */
         if(obstacleGrid == null)
             return 0;
