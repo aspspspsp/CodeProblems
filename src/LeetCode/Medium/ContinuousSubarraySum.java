d18e6419d1afd389a3a998ab3f4c0317d8a061ef
zihanxu <zihan-xu@sohu-inc.com>
2017-08-28 15:59:53 +0800

    fdsfdsf

--- src/LeetCode/Medium/ContinuousSubarraySum.java
+++ src/LeetCode/Medium/ContinuousSubarraySum.java
@@ -5,7 +5,6 @@ public class ContinuousSubarraySum {
         
         //考慮k為0的情況
         if(k == 0) {
-            int numOf0 = 0;
             for(int i = 0; i < nums.length - 1; i ++) {
                 if(nums[i] == 0 && nums[i + 1] == 0)
                     return true;
