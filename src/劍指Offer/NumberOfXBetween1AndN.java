13e23aad0a19cccdc94def4c46ed7171b789c10b
ZiHan <ff@ys.com>
2017-09-05 00:20:32 +0800

    fdsfsdf

--- src/劍指Offer/NumberOfXBetween1AndN.java
+++ src/劍指Offer/NumberOfXBetween1AndN.java
@@ -10,7 +10,8 @@ public class NumberOfXBetween1AndN {
 	public static int numberOfXBetween1AndN_2(int n, int x) {
 		int count = 0;
 		for (int i = 1; i <= n; i *= 10) {
-	        int a = n / i, b = n % i;
+	        int a = n / i;
+	        int b = n % i;
 	        //之所以补8，是因为当百位为0，则a/10==(a+8)/10，
 	        //当百位>=2，补8会产生进位位，效果等同于(a/10+1)
 	        count += (a + 8) / 10 * i + ((a % 10 == 1) ? b + 1 : 0);
