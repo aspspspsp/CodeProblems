fba2d2869ef8ab353b51c503fe2797c580f3509b
xzz64 <aspspspsp@yahoo.com.tw>
2017-10-10 14:50:35 +0800

    fdsfdsf

---
+++ src/LeetCode/Easy/MaxConsecutiveOnes.java
@@ -0,0 +1,28 @@
+package LeetCode.Easy;
+
+public class MaxConsecutiveOnes {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        if(nums == null || nums.length == 0)
+            return 0;
+        
+        int[] results = new int[nums.length];
+        int max = 0;
+        
+        if(nums[0] == 1) {
+            max = 1;
+            results[0] = 1;
+        }
+        
+        for(int i = 1; i < nums.length; i ++) {
+            if(nums[i] == 1) {
+                results[i] = results[i - 1] + 1;
+                max = Math.max(results[i], max);
+            } else
+                results[i] = 0;
+        }
+        
+        return max;
+    }
+}fdsfdsf {
+
+}
