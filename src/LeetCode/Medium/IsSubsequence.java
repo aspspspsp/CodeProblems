77fb7d436506095c8d1e12757e6da6193b93816d
aspspspsp <aspspspsp@yahoo.com.tw>
2018-08-16 16:55:33 +0800

    Create IsSubsequence.java

---
+++ src/LeetCode/Medium/IsSubsequence.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if(s == null || s.equals(""))
+            return true;
+        
+        if(t == null || t.equals(""))
+            return false;
+        
+        
+        char[] ss = s.toCharArray();
+        char[] tt = t.toCharArray();
+        
+        int j = 0;
+        for(int i = 0; i < tt.length; i ++) {
+            if(tt[i] == ss[j]) {
+                j ++;
+            }
+            if(j == ss.length)
+                return true;
+        }
+        
+        if(j == ss.length)
+            return true;
+        
+        return false;
+    }
+}
