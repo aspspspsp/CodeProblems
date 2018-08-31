1934b32169e07f122da260b943463a6757b4f561
aspspspsp <aspspspsp@yahoo.com.tw>
2018-08-31 16:58:06 +0800

    亂碼部分修正

--- src/LeetCode/Medium/VerifyPreorderSerializationOfABinaryTree.java
+++ src/LeetCode/Medium/VerifyPreorderSerializationOfABinaryTree.java
@@ -1,5 +1,7 @@
 package src.LeetCode.Medium;
 /*
+檢查一個二叉樹序列化後的字串是否可以被解析成二叉數(前序歷遍)
+
 0 	   _9_
 1     /   \
 2    3     2
@@ -8,8 +10,8 @@ package src.LeetCode.Medium;
 5 / \ / \   / \
 6 # # # #   # #
 
-��̖  0  1  2  3  4  5  6  7  8  9  10 11 12
-����"9, 3, 4, #, #, 1, #, #, 2, #, 6, #, #"
+index   0  1  2  3  4  5  6  7  8  9  10 11 12
+元素    "9, 3, 4, #, #, 1, #, #, 2, #, 6, #, #"
  * 
  *  ���Y�c(root-0)��9(��0��Ԫ��)
  *  ���Ә�(root-1) (��1��Ԫ��)
@@ -18,51 +20,45 @@ package src.LeetCode.Medium;
  * 
  */
 public class VerifyPreorderSerializationOfABinaryTree {
-	
-	public static void main(String [] args) {
-		VerifyPreorderSerializationOfABinaryTree s = new VerifyPreorderSerializationOfABinaryTree();
-		s.isValidSerialization("9,3,4,#,#,1,#,#,2,#,6,#,#");
-	}
-	
 	public boolean isValidSerialization(String preorder) {
-		//�з�ÿ��Ԫ��
+		//切分每個元素
 		String [] preOrders = preorder.split(",");
-		
-		//�ĵ�0��Ԫ���_ʼ���L������K���صĴ𰸞鱾�������Ԫ�ص��L�ȣ��˘��Ϸ�
+
+		//從第0個元素開始拜訪，若最終返回的答案為本身的所有元素的長度，此樹為合法
 		if(findTree(preOrders, 0) == preOrders.length)
 			return true;
-		//��֮���Ϸ�
+		//反之不合法
 		else
 			return false;
 	}
-	
-	//preOrder���з�������� start��ĵڎׂ�Ԫ���_ʼ���L
+
+	//preOrder為切分後的序列 start為從第幾個元素開始
 	int findTree(String[] preOrders, int start) {
-				
-		//��Ŀǰ�ѽ����L����ĩ�˵��s����"#"���t��ʾ���Ә䲻�Ϸ�(���^�m�z������e)
+
+		//若目前拜訪的元素序號start超過序列長度，則表示不存在這個子樹(子樹長度為-1)
 		if(preOrders.length - start == 0)
-			//-1��ʾ���Ә䲻�Ϸ����������^�m�z����
+			//-1表示此子樹不合法，不必再繼續檢查了
 			return -1;
-		
-		//��Ŀǰ���L��Ԫ��(preOrder[start])��"#"����ʾ�ѽ������ӵ��~�ӹ��c
+
+		//若目前拜訪的元素(preOrder[start])為"#"，表示已經到最下層的葉子節點
 		if(preOrders[start].equals("#"))
-			//�t��ʾ�Θ�����һλ
+			//則表示游標往右一位
 			return start + 1;
-		
-		//�z������������Ŀǰ(start)��1��Ԫ�ؿ�ʼ���𵽵�x��Ԫ�أ�
+
+		//檢查左子树（从目前(start)第1个元素开始算起到第x个元素）
 		int left = findTree(preOrders, start + 1);
-		//�����������Ә�(���Ә��L�� == 0)
+		//若不存在左子樹(左子樹長度 == 0)
 		if(left < 0)
-			//-1��ʾ���Ә䲻�Ϸ����������^�m�z����
+			//-1表示此子樹不合法，不必再繼續檢查了
 			return -1;
-		
-		//�z�����������ӵ�x + 1��Ԫ����������ĩβ��
+
+		//檢查右子树（从第x + 1个元素算起到序列末尾）
 		int right = findTree(preOrders, left);
-		//�����������Ә�(���Ә��L�� == 0)
+		//若不存在右子樹(右子樹長度 == 0)
 		if(right < 0)
-			//-1��ʾ���Ә䲻�Ϸ����������^�m�z����
+			//-1表示此子樹不合法，不必再繼續檢查了
 			return -1;
-		//����һ�����Ә�Ĺ��c��̖(��ʾĿǰ��ֹ�@�Ø䶼�ǺϷ���)
+		//返回一個右子樹的節點編號(表示目前為止這棵樹都是合法的)
 		return right;
 	}
 }
