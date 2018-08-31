cb1f6af0fabebf41433c529536cd58641e13b675
aspspspsp <aspspspsp@yahoo.com.tw>
2018-08-31 14:22:25 +0800

    修正亂碼

--- src/LeetCode/Easy/MinimumDepthOfBinaryTree.java
+++ src/LeetCode/Easy/MinimumDepthOfBinaryTree.java
@@ -11,37 +11,37 @@ import src.LeetCode.Dependencies.TreeNode;
  * }
  */
 
-//���}�c�Ҍ������Ȳ�ͬ����
-//�����Ȟ� max = 0
-//��С��Ȟ� min = Integer.max
+//歷遍整棵樹，然後到最末節點返回深度，再選擇一個最小的深度，即可
 public class MinimumDepthOfBinaryTree {
     public int minDepth(TreeNode root) {
-        
-        //�z���Ƿ�һ�_ʼ�͞��
+        //處理例外情況
         if(root == null)
             return 0;
-        
-        //���Ҷ��]�к��ӱ�ʾ���~�ӹ��c����ʾ����׌ӣ���׌ӵ���Ȟ�1
+
+        int minimum_depth = helper(root, 1);
+        return minimum_depth;
+    }
+
+    int helper(TreeNode root, int curDepth) {
+        //若已經是最末節點，則返回深度
         if(root.left == null && root.right == null)
-            return 1;
-            
-        //Integer.MAX_VALUE ��һ��ӛ̖����ʾ�˹��c���
-        int minLeft = Integer.MAX_VALUE;
-        int minRight = Integer.MAX_VALUE;
-        
-        //�����Ǳ��⌦ƫб����F�e�`�Ĵ�ex:
-        /*
-            1           expect:2
-           / \          wrong answer:1
-         null 2
-        */
-        if(root.left != null) 
-            minLeft = minDepth(root.left);
-            
-        if(root.right != null) 
-            minRight = minDepth(root.right);
-        
-        //�����҃��Ә��ҵ���С�����
-        return Math.min(minLeft, minRight) + 1;
+            return curDepth;
+
+        //紀錄最大深度
+        int left_depth = Integer.MAX_VALUE;
+        int right_depth = Integer.MAX_VALUE;
+
+        //若左邊有子節點則繼續歷遍，並且返回得到左邊子樹的最淺深度
+        if(root.left != null)
+            left_depth = helper(root.left, curDepth + 1);
+
+        //若右邊有子節點則繼續歷遍，並且返回得到左邊子樹的最淺深度
+        if(root.right != null)
+            right_depth = helper(root.right, curDepth + 1);
+
+        int minimum_depth = Math.min(left_depth, right_depth);
+
+        //返回最小深度
+        return minimum_depth;
     }
 }
