26f4345598c084ad3e40805ca13462ff4d0396a3
aspspspsp <aspspspsp@yahoo.com.tw>
2018-08-31 14:55:30 +0800

    修正亂碼

--- src/LeetCode/Easy/IsPowerOfFour.java
+++ src/LeetCode/Easy/IsPowerOfFour.java
@@ -1,25 +1,29 @@
 package src.LeetCode.Easy;
 /*
- * �Q�׹�ʽ loga b = lognb / logn a
+ *首先先利用這個公式log(a,b) = log(n,b) / log(n,a)
  */
 public class IsPowerOfFour {
 	 public boolean isPowerOfFour(int num) {
 	        if(num < 0)
 	            return false;
 	        
-	        //log4 Num(��num��4���)
-	        //���M�ГQ��
+	        /*
+	        因為log(10, 4)/log(10,4)若是整數的話，表示該數可以被4整除
+	        所以我們利用這個性質，先取得整數，再取得小數，相減為0表示
+	        log(10, 4)/log(10,4)為整數，即可被4整除，反之則無法被4整
+	        除
+		 	*/
 	        
-	        //log4 Num ����С���c��λ
+	        //log4 Num 取得 log(10,4) / log(10,4) 整數
 	        double integerLog = (int)(Math.log10(num) / Math.log10(4));
 	        
-	        //log4 Num ��С���c��λ
+	        //log4 Num 取得 log(10,4) / log(10,4) 小數
 	        double log = Math.log10(num) / Math.log10(4);
 	        
-	        //��С���c�Ĝpȥ����С���c�Ğ�0��ʾ�˔�4�Ĵη���
+	        //可被4整除的情況[log(10, 4)/log(10,4)為整數]
 	        if(integerLog - log == 0)
 	        	return true;
-	        //��t�K����
+	        //無法被4整除的情況[log(10, 4)/log(10,4)為小數]
 	        else 
 	        	return false;
 	 }
