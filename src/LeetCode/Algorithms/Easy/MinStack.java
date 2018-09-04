ad65aa0fd6a63d3f57613f17f85b008bee6f311b
aspspspsp <aspspspsp@yahoo.com.tw>
2018-09-04 09:51:41 +0800

    修正亂碼

--- src/LeetCode/Algorithms/Easy/MinStack.java
+++ src/LeetCode/Algorithms/Easy/MinStack.java
@@ -1,45 +1,57 @@
 package src.LeetCode.Algorithms.Easy;
 
 import java.util.Stack;
-
+/*
+在O(1)的時間複雜度取得Stack中的「最小值」資料
+要實作出MinStack的關鍵就是在一個class MinStack中使用「兩個Stack」，
+其中一個Stack用來存放資料(稱為datastack)，另一個用來記錄「目前最小值
+」(稱為minstack)。
+ */
 public class MinStack {
-	
-	//�惦Ԫ�صė�
-    Stack<Integer> elements;
+
+    //存放資料(稱為datastack)
+    Stack<Integer> datastack;
     
-    //�惦��СԪ�صė�
-    Stack<Integer> min;
+    //用來記錄目前最小值(稱為minstack)。
+    Stack<Integer> minstack;
     /** initialize your data structure here. */
     public MinStack() {
-        elements = new Stack<Integer>();
-        min = new Stack<Integer>();
+        minstack = new Stack<Integer>();
+        minstack = new Stack<Integer>();
     }
     
     public void push(int x) {
-        elements.push(x);
-        
-        //������СԪ��(x<=��С)�t������С��
-        if(min.isEmpty() == true || min.peek() >= x)
-            min.push(x);
+        datastack.push(x);
+
+/*
+minstack就必須判斷「欲新增的資料」是否有比「目前最上面」還要小。
+如果有，就把「欲新增的資料」push()進minstack，此時該資料會位在Stack的「最上面」。
+特例：如果原先minstack裏面沒有資料，那麼就直接將「欲新增的資料」push()進minstack。
+透過這個步驟，可以保證minstack的「最上面」資料就是datastack中的「最小值」。
+如果沒有，就把minstack「目前最上面」的資料，再push()進minstack一次，表示在新增資料
+後，「最小值」不變。
+*/
+        if(minstack.isEmpty() == true || minstack.peek() >= x)
+            minstack.push(x);
     }
     
     public void pop() {
-        if(elements.isEmpty() == true)
+        if(datastack.isEmpty() == true)
             return;
         
-        int popVal = elements.peek();
-        if(popVal == min.peek())
-            min.pop();
-        
-        elements.pop();
+        int popVal = datastack.peek();
+        if(popVal == minstack.peek())
+            minstack.pop();
+
+        datastack.pop();
     }
     
     public int top() {
-        return elements.peek();
+        return datastack.peek();
     }
     
     public int getMin() {
-        return min.peek();
+        return minstack.peek();
     }
 }
 
--- src/LeetCode/Algorithms/Easy/NumberOfSegmentsInAString.java
+++ src/LeetCode/Algorithms/Easy/NumberOfSegmentsInAString.java
@@ -1,14 +1,14 @@
 package src.LeetCode.Algorithms.Easy;
 public class NumberOfSegmentsInAString {
     public int countSegments(String s) {
-        //��ȥ���^β�հף����↖�}
+        //先去除頭尾空白，避免問題
         String trimmed = s.trim();
         
         if (trimmed.length() == 0)
             return 0;
         else {
-            //s+ --->���ٳ���һ���ո�
-            //�ָ�ֻ��һ���ո���ִ�������Ӌ��ո��e�`
+            //s+ --->至少出现一个空格
+            //分割只有一個空格的字串，避免計算空格錯誤
             int segmentCounts = trimmed.split("\\s+").length;
             return segmentCounts;
         }
