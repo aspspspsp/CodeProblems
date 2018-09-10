e8e32fda065ee4caacde8857df26678d800e010b
aspspspsp <aspspspsp@yahoo.com.tw>
2018-09-10 10:49:03 +0800

    修正亂碼

--- src/LeetCode/Algorithms/Medium/DesignTwitter.java
+++ src/LeetCode/Algorithms/Medium/DesignTwitter.java
@@ -19,45 +19,45 @@ public class Twitter {
     }
     
     int timeStampCounter;
-    
-    //ÿ���˰l������Ϣ(�Ñ�ID, tweet)
+
+	//每個人發布的消息(用戶ID, tweet)
     HashMap<Integer, List<Tweet>> timelines;
-    
-	//set�e���ŵă��ݶ�������ͬ����ʹ�����������Ñ�׷ۙĳ�Ñ�
-    //�o����H�P�S(�Ñ�ID, ���Ñ�����׷ۙ���Ñ�IDs)
+
+	//set裡面存放的內容都不會相同，故使用它來作為用戶追蹤某用戶
+	//紀錄人際關係(用戶ID, 此用戶正在追蹤的用戶IDs)
     HashMap<Integer, HashSet<Integer>> relation;
     
     /** Initialize your data structure here. */
-    //��ʼ��
+	//初始化
     public Twitter() {
         this.timelines = new HashMap<Integer, List<Tweet>>();
         this.relation = new HashMap<Integer, HashSet<Integer>>();
     }
-    
-    //poһ����tweet
+
+	//po一個新tweet
     /** Compose a new tweet. */
     public void postTweet(int userId, int tweetId) {
-    	//���]��ԓ�Ñ�t����һ��
+		//若沒有該用戶則新增一個
         if(timelines.containsKey(userId) == false) {
         	timelines.put(userId, new ArrayList<Tweet>());
         }
-        
-        //�M��po tweet�Ą���
+
+		//進行po tweet的動作
         timelines.get(userId).add(new Tweet(tweetId, timeStampCounter));
         timeStampCounter ++;
     }
     
     /** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
-    //��ÿ��׷ۙ���Ñ�ȡ��10��(�����Լ�Ҳȡ��10��)tweet���ٌ�����ȡ����tweet����������
-    //��θ�ȡ��10��������Ñ����ܕ��B�m�l��10���µ�����
+	//對每個追蹤的用戶個取出10個(包含自己也取出10個)tweet，再將所有取出的tweet再另作排序
+	//為何各取出10個，因為用戶可能會連續發出10最新的文章
     public List<Integer> getNewsFeed(int userId) {
-    	//�����Ñ���׷ۙ�������Ñ�ȡ��
+		//將此用戶所追蹤的所有用戶取出
     	HashSet<Integer> followees = relation.get(userId);
-    	
-    	//�Á��ź��x��tweet
+
+		//用來存放候選的tweet
     	List<Tweet> candiciates = new ArrayList<Tweet>();
-    	
-    	//ȡ���Լ�post��tweet
+
+		//取出自己post的tweet
 		List<Tweet> userTimeline = timelines.get(userId);
     	if(userTimeline != null) {
     		int lowerBound = Math.max(0, userTimeline.size() - 10);
@@ -65,13 +65,13 @@ public class Twitter {
     			candiciates.add(userTimeline.get(i));
     		}
     	}
-    	
-    	//ȡ������׷ۙ���Ñ�����post��tweet
+
+		//取出所有追蹤的用戶最新post的tweet
     	if(followees != null) {
     		for(Integer followee : followees) {
     			List<Tweet> followeeTimeline = timelines.get(followee);
-    			
-    			//�����Ñ��]�аl���κε�tweet���t���^
+
+				//若此用戶沒有發表任何的tweet，則跳過
     			if(followeeTimeline == null)
     			    continue;
     			    
@@ -81,8 +81,8 @@ public class Twitter {
         		}
     		}
     	}
-    	
-    	//������tweet�M�������ԕr�g�����
+
+		//對所有tweet進行排序，以時間戳為準
     	Collections.sort(candiciates, new Comparator<Tweet>() {
 			@Override
 			public int compare(Tweet t1, Tweet t2) {
@@ -90,8 +90,8 @@ public class Twitter {
 				return t2.timeStamp - t1.timeStamp;
 			}
     	});
-    	
-    	//�����µ�10�P�Y��ݔ��
+
+		//將最新的10筆結果輸出
     	List<Integer> result = new ArrayList<Integer>();
     	int upperBound = Math.min(10, candiciates.size());
     	for(int i = 0; i < upperBound; i++ ) {
@@ -102,32 +102,32 @@ public class Twitter {
     }
     
     /** Follower follows a followee. If the operation is invalid, it should be a no-op. */
-    //����׷ۙ�Ñ�
+	//實作追蹤用戶
     public void follow(int followerId, int followeeId) {
-        //��ֹ�Լ�׷ۙ�Լ�
+		//防止自己追蹤自己
     	if(followerId == followeeId)
         	return;
-    	
-    	//���]���ҵ����Ñ�t����һ��
+
+		//若沒有找到此用戶則新增一個
     	if(relation.containsKey(followerId) == false) {
     		relation.put(followerId, new HashSet<Integer>());
     	}
-    	
-    	//�M��׷ۙ�Ą���
+
+		//進行追蹤的動作
     	relation.get(followerId).add(followeeId);
     }
     
     /** Follower unfollows a followee. If the operation is invalid, it should be a no-op. */
-    //������׷ۙ�Ñ�
+	//實作不追蹤用戶
     public void unfollow(int followerId, int followeeId) {
-    	//ȡ��ԓ�Ñ����е�׷ۙ���Ñ�
+		//取得該用戶所有的追蹤的用戶
     	HashSet<Integer> followees = relation.get(followerId);
-    	
-    	//��ԓ�Ñ��]��׷ۙ�κ��˄t�x�_
+
+		//若該用戶沒有追蹤任何人則離開
     	if(followees == null)
     		return;
-    	
-    	//�M��ȡ��׷ۙ�Ą���
+
+		//進行取消追蹤的動作
     	followees.remove(followeeId);
     }
 }
