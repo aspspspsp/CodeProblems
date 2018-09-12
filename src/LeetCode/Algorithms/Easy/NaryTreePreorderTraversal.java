5de793f834c6dbae2f4b4c1ae6a75afd4ca3e1d1
aspspspsp <aspspspsp@yahoo.com.tw>
2018-09-12 14:46:57 +0800

    N-ary Tree Preorder Traversal

---
+++ src/LeetCode/Algorithms/Easy/NaryTreePreorderTraversal.java
@@ -0,0 +1,44 @@
+package src.LeetCode.Algorithms.Easy;
+
+import src.LeetCode.Algorithms.Dependencies.Node;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+public class NaryTreePreorderTraversal {
+    public List<Integer> preorder(Node root) {
+        List<Integer> result = new ArrayList<>();
+        if(root == null)
+            return result;
+
+        helper(root, result);
+
+        return result;
+    }
+
+    void helper(Node root, List<Integer> result) {
+        // 加入當前元素
+        result.add(root.val);
+
+        // 歷遍所有子節點
+        if(root.children != null) {
+            for(int i = 0; i < root.children.size(); i ++) {
+                helper(root.children.get(i), result);
+            }
+        }
+    }
+}
