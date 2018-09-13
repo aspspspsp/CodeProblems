317867a8fbf8706170d0b9f64367ef0de43343ab
aspspspsp <aspspspsp@yahoo.com.tw>
2018-09-13 15:10:53 +0800

    做了優化

--- src/LeetCode/Algorithms/Easy/JewelsAndStones.java
+++ src/LeetCode/Algorithms/Easy/JewelsAndStones.java
@@ -1,5 +1,8 @@
 package src.LeetCode.Algorithms.Easy;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class JewelsAndStones {
     public int numJewelsInStones(String J, String S) {
         if(J == null || J.length() == 0)
@@ -11,12 +14,15 @@ public class JewelsAndStones {
         char[] js = J.toCharArray();
         char[] ss = S.toCharArray();
 
+        Set set = new HashSet<Character>();
+        for(int i = 0; i < js.length; i ++) {
+            set.add(js[i]);
+        }
+
         int result = 0;
         for(int i = 0; i < ss.length; i ++) {
-            for(int j = 0; j < js.length; j ++) {
-                if(js[j] == ss[i])
-                    result ++;
-            }
+            if(set.contains(ss[i]))
+                result ++;
         }
 
         return result;
