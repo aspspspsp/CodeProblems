8df6f7cdb2f65597ae91517f66148dbb5d4a55ee
aspspspsp <aspspspsp@yahoo.com.tw>
2018-09-17 11:25:59 +0800

    翻譯說明

--- src/LeetCode/Algorithms/Medium/LinkedListComponents.java
+++ src/LeetCode/Algorithms/Medium/LinkedListComponents.java
@@ -7,9 +7,18 @@ import java.util.Set;
 
 public class LinkedListComponents {
 /*
-Scanning through the list, if node.val is in G and node.next.val isn't (including if node.next is null), then this must be the end of a connected component.
-
-For example, if the list is 0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7, and G = [0, 2, 3, 5, 7], then when scanning through the list, we fulfill the above condition at 0, 3, 5, 7, for a total answer of 4.
+掃描整個鍊表，如果符合下列所有條件:
+1.當前結點的值有在G當中
+2.下一個節點的值不在G中或者下一個節點為null
+則表示這個節點為一個連通分量(connected component)的結尾。
+例如：
+list:
+0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7
+G:
+[0, 2, 3, 5, 7]
+歷遍完結點後，我們找到了符合上述條件的節點如下:
+0, 3, 5, 7
+所以答案為4
 */
     public int numComponents(ListNode head, int[] G) {
 
@@ -21,10 +30,12 @@ For example, if the list is 0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7, and G = [0, 2,
 
         int ans = 0;
         while (head != null) {
+            // 檢查是否符合條件，是的話+1
             if(set.contains(head.val) == true &&
                     (head.next == null || set.contains(head.next.val) == false))
                 ans ++;
 
+            // 移動指針
             head = head.next;
         }
 
