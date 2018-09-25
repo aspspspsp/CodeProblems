42ec70d194493d4007b561202eeb3700898cb940
aspspspsp <aspspspsp@yahoo.com.tw>
2018-09-25 11:17:20 +0800

    增加說明

--- src/LeetCode/Algorithms/Easy/MinCostClimbingStairs.java
+++ src/LeetCode/Algorithms/Easy/MinCostClimbingStairs.java
@@ -2,6 +2,7 @@ package src.LeetCode.Algorithms.Easy;
 
 public class MinCostClimbingStairs {
     /*
+    如果我?用一???dp[]?存放到?每一?所需要的花?值。??最??果是求dp[cost.length]的值。因?每次可以走1?或者2?，并且可以?0或者?1?始，所以可以得到dp[0]?0，dp[1]?0。?2?始，dp[i]可以由通?dp[i-2]走2?或者通?dp[i-1]走一?到?，而?i-2和i-1?所要花?的值分??cost[i-2]和cost[i-1]，所以，dp[i] = min(dp[i-2] + cost[i-2], dp[i-1] + cost[i-1])。?算法的空?复?度?O(n)，??复?度?O(n)。
     dp[i] = min(dp[i-2] + cost[i-2], dp[i-1] + cost[i-1])
     */
     public int minCostClimbingStairs(int[] cost) {
